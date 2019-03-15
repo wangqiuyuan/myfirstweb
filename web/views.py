357da0a5ac4634fc36b067e4703657231a12459e
wangqiuyuan <wangqiuyuan@58.com>
2019-03-15 20:57:10 +0800

    web,first commit

---
+++ web/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+# Create your views here.
+def  hello(request):
+    #return HttpResponse("Hello,Django!")
+    return render(request,"index.html",)
